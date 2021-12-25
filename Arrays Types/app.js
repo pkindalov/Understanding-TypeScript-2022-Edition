c26c7144db7b74dac34bcd6479ce62e46ae1b492
pkindalov <panayotkindalov@gmail.com>
2021-12-26 00:40:04 +0200

    Updated gitignore. Added examples about Arrays Types abd WIorking with tuples.

---
+++ Arrays Types/app.js
@@ -0,0 +1,16 @@
+// const person: {   //not good practice at all
+//     name: string;
+//     age: number;
+// } = {
+var person = {
+    name: 'Maximilian',
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
+};
+var favoriteActivities;
+favoriteActivities = ['Sports'];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
