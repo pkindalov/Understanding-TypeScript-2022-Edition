6a16fd5c392780abea10229f45f5cec636462430
pkindalov <panayotkindalov@gmail.com>
2023-02-05 18:22:32 +0200

    Added folder Function return types and void and updated gitignore

---
+++ Function Return Types And Void/app.js
@@ -0,0 +1,12 @@
+// JUST AS WITH VARIABLES, IT’S A GOOD IDEA TO LET TIME DO ITS JOB REGARGING TYPE INFERENCE. 
+// AND IF YOU HAVE NO SPECIFIC REASON FOR EXPLICITLY SETTING THE TYPE, 
+// YOU SHOULD THEREFORE NOT SET THE TYPE AND INSTEAD LET TYPESCRIPT INFER TO TYPE.
+function add(n1, n2) {
+    return n1 + n2;
+}
+//void means that this function doesn't have a return statement.
+//It doesn't return anything.
+function printResult(num) {
+    console.log('Result: ' + num);
+}
+printResult(add(5, 12));
